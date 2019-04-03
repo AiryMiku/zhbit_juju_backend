2a9e5191a221b9be84be4fd00340fafbf775bb7b
tangpeihua <764776576@qq.com>
2019-04-03 13:59:05 +0800

    add  message urls

--- apps/http/message/urls.py
+++ apps/http/message/urls.py
@@ -7,9 +7,13 @@
 from django.urls import path, include
 
 from apps.http.message.controller import MessageController
+from apps.http.message.controller import SessionController
 
 app_name = "message"
 
 urlpatterns = [
-    path('create_message/', MessageController.create_message)
+    path('create_message/', MessageController.create_message),
+    path('get_message_list_by_session_id/', MessageController.get_message_list_by_session_id),
+    path('create_session/', SessionController.create_session),
+    path('get_session_list/', SessionController.get_session_list),
 ]
