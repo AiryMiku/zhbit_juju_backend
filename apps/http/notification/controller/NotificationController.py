d0bb75f5eb23454c2d38f8d1215130f48a2d1337
tangpeihua <764776576@qq.com>
2019-04-14 16:28:59 +0800

    msgs

--- apps/http/notification/controller/NotificationController.py
+++ apps/http/notification/controller/NotificationController.py
@@ -12,19 +12,21 @@ from apps.http.decorator.LoginCheckDecorator import request_check
 from apps.http.user.controller import UtilsController
 
 
-def create_notification(notification_type,to_id,content):
+@request_check()
+def create_notification(notification_type, to_id, content):
     # 推送消息的类型 0 = 系统 1 = 给用户发 2 = 给群组的所有用户 3 = 给群组的管理员 4 = 给参与活动的人
-    models.Notification.objects.create(notification_type=notification_type,to_id=to_id,notification_content=content)
+    models.Notification.objects.create(notification_type=notification_type, to_id=to_id,notification_content=content)
 
 
+@request_check()
 def get_notification_list(request: HttpRequest):
-    _param = validate_and_return(request,{
-        'access_token':'',
+    _param = validate_and_return(request, {
+        'access_token': '',
     })
 
     user_id = UtilsController.get_id_by_token(_param['access_token'])
     if user_id == -1:
-        return rS.fail(rS.ReturnResult.UNKNOWN_ERROR,'此账号已在别处登录')
+        return rS.fail(rS.ReturnResult.UNKNOWN_ERROR, '此账号已在别处登录')
 
     queryset = models.Notification.objects.all().order_by("-create_time")
 
@@ -40,19 +42,19 @@ def get_notification_list(request: HttpRequest):
                 dict_data[str(k.id)] = k.to_list_dict()
 
         if k.notification_type == 2:
-            if models.UserFollowGroupMapping.objects.filter(group=k.to_id,user=user_id):
+            if models.UserFollowGroupMapping.objects.filter(group=k.to_id, user=user_id):
                 dict_data.setdefault(str(k.id))
                 dict_data[str(k.id)] = k.to_list_dict()
 
         if k.notification_type == 3:
-            if models.UserFollowGroupMapping.objects.filter(group=k.to_id,user=user_id):
-                if models.UserFollowGroupMapping.objects.get(group=k.to_id,user= user_id).role == 1 |     \
-                        models.UserFollowGroupMapping.objects.get(group=k.to_id,user= user_id).role == 2:
+            if models.UserFollowGroupMapping.objects.filter(group=k.to_id, user=user_id):
+                if models.UserFollowGroupMapping.objects.get(group=k.to_id, user=user_id).role == 1 |     \
+                        models.UserFollowGroupMapping.objects.get(group=k.to_id, user=user_id).role == 2:
                     dict_data.setdefault(str(k.id))
                     dict_data[str(k.id)] = k.to_list_dict()
 
         if k.notification_type == 4:
-            if models.UserAttendActivityMapping.objects.filter(user=user_id,activity=k.to_id):
+            if models.UserAttendActivityMapping.objects.filter(user=user_id, activity=k.to_id):
                 dict_data.setdefault(str(k.id))
                 dict_data[str(k.id)] = k.to_list_dict()
 
