5dae87bf9d78cb0f3df711c826c4da2d78118fa2
airy <532710813@qq.com>
2019-05-08 02:18:59 +0800

    Revert "very stupid to user online database"
    This reverts commit c7ffc14d32252b5d503de8a72d31d072378f4168.

--- zhbit_juju_backend/settings.py
+++ zhbit_juju_backend/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'NAME': 'juju',
         'USER': 'root',
         'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
+        'HOST': '120.24.16.183',
         'PORT': '3306',
     }
 }
